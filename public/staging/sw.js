0b8948e8892a2b05e98db95061d6b27bf12503eb
OPIYO OSCAR <opiyooscar414@gmail.com>
2026-08-22 22:50:14 +0300

    deploy(web): staging build v5.4.0 under public/staging

--- public/staging/sw.js
+++ public/staging/sw.js
@@ -8,7 +8,7 @@
  * | API mutations  | Network (pass-through)         | App queues IndexedDB |
  */
 
-const CACHE_VERSION = 'v5.4.0-65b176e';
+const CACHE_VERSION = 'v5.4.0-ce732b9';
 const STATIC_CACHE = `custosell-static-${CACHE_VERSION}`;
 const API_CACHE = `custosell-api-${CACHE_VERSION}`;
 const API_PATH = /\/api\/v1(\/|$)/;
